7c339c7b22c3e5d070d6da0fdcf13ac0ea34a8ba
Unknown <korakot.see@ku.th>
2018-05-31 02:16:34 +0700

    ADD VOICE IN MAP 1 [ Coin && Heart]

--- KhanKluay/Assets/ItemScore.cs
+++ KhanKluay/Assets/ItemScore.cs
@@ -5,21 +5,28 @@ using UnityEngine.UI;
 
 public class ItemScore : MonoBehaviour {
 
+	public AudioClip sound;
+   	public AudioSource source;
+
 	Collider2D c;
 	// Use this for initialization
 	void Start () {
 		c = GetComponent<Collider2D>();
 		c.isTrigger = true;
+		source = GetComponent<AudioSource>();
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player"){
-		Text t;
-		t = GameObject.Find("/Canvas showVeiw/Score").GetComponent<Text>();
-		GameContorler.numScore += 10;
-		t.text = "Score: " + GameContorler.numScore;
-		Destroy(gameObject);
+			Text t;
+			t = GameObject.Find("/Canvas showVeiw/Score").GetComponent<Text>();
+			GameContorler.numScore += 10;
+			t.text = "Score: " + GameContorler.numScore;
+			
+			new Sound().SoundFX(sound, gameObject.transform.position);
+			Destroy(gameObject);
+		
 		}
 	}
 }
