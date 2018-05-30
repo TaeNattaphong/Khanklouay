b81aeef1f77f1f021ab7b038bf8c3e0a9f866b76
Unknown <nattaphong.r@ku.th>
2018-05-30 20:00:21 +0700

    add new Assets of Enemy

---
+++ KhanKluay/Assets/enime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class enime : MonoBehaviour {
+    public float walkSpeed = 1.0f;
+    public float wallLeft = 0.5f;
+    public float wallRight = 0.5f;
+    public float walkingDirection = 0.5f;
+    Vector3 walkAmount;
+    void Update () {
+        walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+         if (walkingDirection > 0.0f && transform.position.x >= wallRight)
+             walkingDirection = -1.0f;
+         else if (walkingDirection < 0.0f && transform.position.x <= wallLeft)
+             walkingDirection = 1.0f;
+        transform.Translate(walkAmount);
+    }
+}
